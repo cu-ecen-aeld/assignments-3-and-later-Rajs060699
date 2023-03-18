183a0ae0a04db95e566b9bd9fc1727d17414eff2
Rajesh Srirangam <rasr7993@colorado.edu>
2023-03-18 14:02:52 -0600

    master:mainfilechange

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -23,7 +23,7 @@
 int aesd_major =   0; // use dynamic major
 int aesd_minor =   0;
 
-MODULE_AUTHOR("Rajesh"); /** TODO: fill in your name **/
+MODULE_AUTHOR("Rajesh"); /** TODO: fill in name **/
 MODULE_LICENSE("Dual BSD/GPL");
 
 struct aesd_dev aesd_device;
